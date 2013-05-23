0d16423627e8f7017087399cff1839247d787484
mpollack <mpollack@vmware.com>
2013-05-23 03:51:22 -0400

    Clean up of doc generation, typos and wrong level sections for TOC

--- gen-docs.sh
+++ gen-docs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p target/docs
-asciidoctor --destination-dir target/docs -a toc -a theme=flask -a copycss ./guide/fullGuide.adoc 
+asciidoctor --destination-dir target/docs -a toc -a theme=flask -a copycss -a source-highlighter=highlightjs ./guide/FullGuide.adoc 
